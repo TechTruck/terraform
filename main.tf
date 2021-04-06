1fabf30a287fdd972a8a26b0233a2746cda9f800
TechTruck <shweta.atrish@gmail.com>
2021-04-05 21:31:48 -0700

    Adding new files

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
-provider "aws" {
-    region = "us-east-1"   
-}
 
 resource "aws_instance" "myec2" {
-    ami = "ami-0742b4e673072066f"
-    instance_type = "t2.micro"
-    tags = {
-      "Name" = "Shweta-Terraform"
-    }
+  count         = var.instanceCount
+  ami           = "${var.amiid}"
+  instance_type = "t2.micro"
+  tags = {
+    # "Name" = "Shweta-Terraform.${count.index + 1}"
+    "Name" =  "Shweta-Terraform-${var.tags[count.index]}"
+  }
 }
 
 output "myec2ipaddress" {
-    value = aws_instance.myec2.public_ip
+  value = aws_instance.myec2[*].public_ip
 }
 
 output "myec2privateip" {
-    value = aws_instance.myec2.private_ip
+  value = aws_instance.myec2[*].private_ip
 }
